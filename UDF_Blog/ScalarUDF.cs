7aeb43d0c220dbd40ea39f4259c9d05b66ab32a6
devilliersdutoit <jacob.dutoit@googlemail.com>
2015-05-17 15:44:45 +0200

    4th

--- UDF_Blog/ScalarUDF.cs
+++ UDF_Blog/ScalarUDF.cs
@@ -18,8 +18,6 @@ public partial class UserDefinedFunctions
         WindowsIdentity currentIdentity = null;
         WindowsImpersonationContext impersonatedIdentity = null;
         if (SqlContext.WindowsIdentity != null)
-
-
         {
             currentIdentity = SqlContext.WindowsIdentity;
             impersonatedIdentity = currentIdentity.Impersonate();
